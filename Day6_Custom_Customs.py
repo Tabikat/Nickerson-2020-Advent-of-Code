80d0c776ea53ae7fe972d7bbb8462c2ff100ac10
Tabikat <66338089+Tabikat@users.noreply.github.com>
2020-12-09 21:42:28 -0700

    Update Day6_Custom_Customs.py

--- Day6_Custom_Customs.py
+++ Day6_Custom_Customs.py
@@ -1,2 +1,26 @@
 import os
+import string
 file=open('Day6.txt').read()
+groups=file.split('\n\n')
+
+some_yes_answers=0
+all_yes_answers=0
+for i in range(0,len(groups)):
+    num_of_people_in_group=groups[i].count('\n')+1
+    groups[i]=groups[i].replace('\n','')
+    #determines all possible answers for the group
+    answers=[]
+    for j in list(groups[i]):
+        if j not in answers:
+            answers.append(j)
+    some_yes_answers+=len(answers)
+    #determines which letters are in every answer
+    nonanswers=[]
+    for k in answers:
+        if groups[i].count(k)!=num_of_people_in_group:
+            nonanswers.append(k)
+    all_yes_answers+=(len(answers)-len(nonanswers))
+
+
+print('Sum of Yes Answers:', some_yes_answers)
+print('Sum of All-Yes Answers:', all_yes_answers)
