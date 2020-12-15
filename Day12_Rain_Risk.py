0d1273c3af6882e0f8501dfae17a5cbada7fd8a9
Tabikat <66338089+Tabikat@users.noreply.github.com>
2020-12-15 00:45:05 -0700

    Update Day12_Rain_Risk.py

--- Day12_Rain_Risk.py
+++ Day12_Rain_Risk.py
@@ -5,8 +5,9 @@ navigation=file.split('\n')
 #Part 1
 hori=0 #track horizontal movement
 vert=0 #tracks vertical movement
-dir=0
+dir=0 #tracks degrees of rotation, where East=0
 for i in navigation:
+    #N&S change vertical location, E&W change horizontal location
     if i[0]=='N':
         vert+=int(i[1:])
     if i[0]=='E':
@@ -15,14 +16,17 @@ for i in navigation:
         vert-=int(i[1:])
     if i[0]=='W':
         hori-=int(i[1:])
+    #L adds degrees, once over 360 degrees, subtract one whole rotation
     if i[0]=='L':
         dir+=int(i[1:])
         if dir>=360:
             dir-=360
+    #R subtracts degrees, once under 0 degrees, add one whole rotation
     if i[0]=='R':
         dir-=int(i[1:])
         if dir<0:
             dir+=360
+    #R F continues in the direction it is facing
     if i[0]=='F':
         if dir==0:
             hori+=int(i[1:])
@@ -41,34 +45,35 @@ vert=0 #tracks vertical movement
 waypt_hori=10 #waypoint horizontal location
 waypt_vert=1 #waypoint vertical location
 for i in navigation:
+    #N&S change vertical location of waypoint, E&W change horizontal location
     if i[0]=='N':
         waypt_vert+=int(i[1:])
-        print((hori,vert),(waypt_hori,waypt_vert))
     if i[0]=='E':
         waypt_hori+=int(i[1:])
-        print((hori,vert),(waypt_hori,waypt_vert))
     if i[0]=='S':
         waypt_vert-=int(i[1:])
-        print((hori,vert),(waypt_hori,waypt_vert))
     if i[0]=='W':
         waypt_hori-=int(i[1:])
-        print((hori,vert),(waypt_hori,waypt_vert))
+    #L rotates points around ship counterclockwise
     if i[0]=='L':
         rotations=int(int(i[1:])/90)
         wh=waypt_hori
         wv=waypt_vert
+        #1 rotation represents 90 degrees
         if rotations==1:
             waypt_hori=wv
             waypt_vert=wh
             waypt_hori=-waypt_hori
+        #2 rotations represents 180 degrees
         if rotations==2:
             waypt_hori=-waypt_hori
             waypt_vert=-waypt_vert
+        #3 rotations represents 270 degrees
         if rotations==3:
             waypt_hori=wv
             waypt_vert=wh
             waypt_vert=-waypt_vert
-        print((hori,vert),(waypt_hori,waypt_vert))
+    #R rotates points around ship clockwise
     if i[0]=='R':
         dir-=int(i[1:])
         if dir<0:
@@ -76,20 +81,22 @@ for i in navigation:
         rotations=int(int(i[1:])/90)
         wh=waypt_hori
         wv=waypt_vert
+        #1 rotation represents 90 degrees
         if rotations==1:
             waypt_hori=wv
             waypt_vert=wh
             waypt_vert=-waypt_vert
+        #2 rotations represents 180 degrees
         if rotations==2:
             waypt_hori=-waypt_hori
             waypt_vert=-waypt_vert
+        #3 rotations represents 270 degrees
         if rotations==3:
             waypt_hori=wv
             waypt_vert=wh
             waypt_hori=-waypt_hori
-        print((hori,vert),(waypt_hori,waypt_vert))
     if i[0]=='F':
         hori+=waypt_hori*int(i[1:])
         vert+=waypt_vert*int(i[1:])
-        print((hori,vert),(waypt_hori,waypt_vert))
+        #Moves ship to the waypoint number of times indicated
 print('Manhattan Distance with Waypoint:', abs(hori)+abs(vert))
