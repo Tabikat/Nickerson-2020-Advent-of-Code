5a829437cbf36ee72b56874587025983f6b28c9d
Tabikat <66338089+Tabikat@users.noreply.github.com>
2020-12-15 00:43:32 -0700

    Update Day15.py

--- Day15.py
+++ Day15.py
@@ -1,30 +1,27 @@
 import os
-input=[1,12,0,20,8,16]
 input_indices={1:[0],12:[1],0:[2],20:[3],8:[4],16:[5]}
 
-while len(input)<30000000:
-    if input.count(input[-1])==1:
+new_number=16
+for i in range(6,30000000):
+    if len(input_indices[new_number])==1:
+        new_number=0
         if len(input_indices[0])==2:
             dictionary_values=input_indices[0]
-            input_indices[0]=[dictionary_values[-1],len(input)]
+            input_indices[0]=[dictionary_values[-1],i]
         else:
-            input_indices[0].append(len(input))
-        input.append(0)
-        print(len(input))
+            input_indices[0].append(i)
     else:
-        previous_indices=input_indices[input[-1]]
+        previous_indices=input_indices[new_number]
         new_number=previous_indices[-1]-previous_indices[-2]
-        if new_number in input:
+        if new_number in input_indices:
             dictionary_values=input_indices[new_number]
             if len(input_indices[new_number])==2:
-                input_indices[new_number]=[dictionary_values[-1],len(input)]
+                input_indices[new_number]=[dictionary_values[-1],i]
             else:
-                input_indices[new_number].append(len(input))
+                input_indices[new_number].append(i)
         else:
-            input_indices[new_number]=[len(input)]
-        input.append(new_number)
-        print(len(input))
-#print(input_indices)
-
-print('2020th number:',input[2019])
-print('30000000th number:',input[29999999])
+            input_indices[new_number]=[i]
+    if i==2019:
+        print('2020th number:',new_number)
+    if i==29999999:
+        print('30000000th number:',new_number)
