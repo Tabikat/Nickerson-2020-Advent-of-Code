1c7d6a95e85b5091cbec48892c26fc6a387a1d70
Tabikat <66338089+Tabikat@users.noreply.github.com>
2020-12-20 23:34:55 -0700

    Update Day5_Binary_Boarding.py

--- Day5_Binary_Boarding.py
+++ Day5_Binary_Boarding.py
@@ -7,13 +7,9 @@ FB=0
 RL=0
 plane_dim=list(boarding_passes[0])
 for i in plane_dim:
-    if i=='F':
+    if i=='F' or i=='B':
         FB+=1
-    elif i=='B':
-        FB+=1
-    elif i=='R':
-        RL+=1
-    elif i=='L':
+    elif i=='R' or i=='L':
         RL+=1
 rows=2**FB #2^7 makes 128 rows
 columns=2**RL #2^3 makes 8 columns
@@ -33,8 +29,8 @@ def upper_half(min,max):
     return (min+half,max)
 
 seatIDs=[]
-for i in range(len(boarding_passes)):
-    one_pass=list(boarding_passes[i]) #list of coordinates for one pass
+for p in boarding_passes:
+    one_pass=list(p) #list of coordinates for one pass
     (min_row,max_row)=(0,rows)
     (min_col,max_col)=(0,columns)
     #determines the row of the seat
@@ -42,24 +38,24 @@ for i in range(len(boarding_passes)):
         if one_pass[j]=='F': #Front
             if max_row-min_row!=2:
                 (min_row,max_row)=lower_half(min_row,max_row)
-            else:
+            else: #for the final split
                 seat_row=lower_half(min_row,max_row)
         if one_pass[j]=='B': #Back
             if max_row-min_row!=2:
                 (min_row,max_row)=upper_half(min_row,max_row)
-            else:
+            else: #for the final split
                 seat_row=upper_half(min_row,max_row)
     #determines the column of the seat
     for j in range(FB,FB+RL):
         if one_pass[j]=='L': #Left
             if max_col-min_col!=2:
                 (min_col,max_col)=lower_half(min_col,max_col)
-            else:
+            else: #for the final split
                 seat_col=lower_half(min_col,max_col)
         if one_pass[j]=='R': #Right
             if max_col-min_col!=2:
                 (min_col,max_col)=upper_half(min_col,max_col)
-            else:
+            else: #for the final split
                 seat_col=upper_half(min_col,max_col)
     seatIDs.append(seat_row*8+seat_col)
 print('Lowest Seat ID:', min(seatIDs))
